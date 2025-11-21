40b816bb1cfb6605cb7f0abb48b3a7ff88dd9219
Misha Balakhno <mbalakhno@openai.com>
2025-11-21 09:39:18 -0800

    Fixed lifetime elision warning

--- src/usda/mod.rs
+++ src/usda/mod.rs
@@ -44,7 +44,7 @@ impl sdf::AbstractData for TextReader {
         self.data.get(path).map(|spec| spec.ty)
     }
 
-    fn get(&mut self, path: &sdf::Path, field: &str) -> Result<Cow<sdf::Value>> {
+    fn get(&mut self, path: &sdf::Path, field: &str) -> Result<Cow<'_, sdf::Value>> {
         let Some(spec) = self.data.get(path) else {
             bail!("No spec found for path: {}", path)
         };
